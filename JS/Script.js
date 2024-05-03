3f0a614021a9d940e1e6137799ca3466cdd08e0b
Yordanos G. Tefera <gt.yordanos@gmail.com>
2024-05-03 14:33:56 +0300

    I added the scroller button and the Footer

--- JS/Script.js
+++ JS/Script.js
@@ -2,3 +2,8 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
   header.classList.toggle("sticky", window.scrollY>80);
 });
+
+const scroller = document.querySelector(".scroll");
+  window.addEventListener("scroll", function () {
+    scroller.classList.toggle("scrollTop", window.scrollY > 190);
+  });
